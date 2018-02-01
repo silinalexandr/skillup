2b10d4df30f25b989fb1dc3d3111096c05cfb7d0
SkillUP Student <student@skillup.spb.ru>
2018-02-01 21:37:35 +0300

    commit remove

---
+++ head.php
@@ -0,0 +1,3 @@
+    <title><?= PROJECT_NAME ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 <?php
 
+define('SECOND_IN_DAY', 86400);
+define('PROJECT_NAME', "SkillUp" );
+
 $currentDate = time();
-$yesterday = $currentDate - 60*60*24;
+$yesterday = $currentDate - SECOND_IN_DAY*7;
 
 echo $currentDate . "<br />";
 echo date('d.m.Y H:i:s') . "<br />";
@@ -14,3 +17,74 @@ require_once 'template.php'
 ?>
 
 <p>Some text..  <?= $currentDate ?> </p>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once 'head.php'; ?>
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+<h4> Регистрация </h4>
+
+<form action="template.php" method="POST">
+    <label>Имя:</label>
+    <div id="block_firstname" class="form-group">
+        <input type="text" name="firtname">
+    </div>
+    <label>Фамилия:</label>
+
+    <div class="form-group">
+        <input type="text" name="lastname">
+    </div>
+
+    <label>Пароль:</label>
+    <div class="form-group">
+        <input type="text" name="password">
+    </div>
+
+    <label>Пол</label>
+    <div>
+        Муж<input type="checkbox" name="sex">
+        Жен<input type="checkbox" name="sex">
+    </div>
+
+    <label>Курите</label>
+    <div>
+        да<input type="checkbox" name="smoke">
+        нет<input type="checkbox" name="smoke">
+    </div>
+
+    <label>о себе:</label>
+    <div>
+        <textarea name="about" rows="3"> </textarea>
+    </div>
+
+    <label for="city">Город:</label>
+    <div>
+        <select name="city">
+            <option>Москва</option>
+            <option>СПб</option>
+        </select>
+    </div>
+
+    <div>
+        <button>Сохранить</button>
+    </div>
+
+
+</form>
+
+</body>
+</html>
